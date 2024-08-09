a5c14381a42f01c9e74992c10b87ab18fa512c95
myluqman <luqman.ku@gmail.com>
2024-08-09 22:41:23 +0700

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import (
         QHBoxLayout, QVBoxLayout, QGridLayout, 
         QGroupBox, QRadioButton,
         QPushButton, QLabel, QListWidget, QLineEdit)
-        
+        ############################################################
 from instr import *
 
 class FinalWin(QWidget):
